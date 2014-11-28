ec2f321573559aa6cb06a9d5c1a237bbd2b588eb
Joey wang <wxluckly@gmail.com>
2014-11-28 18:04:16 +0800

    coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :staffers
   root 'videos#index'
 
   resource :biography
   resource :contact
   resources :photos
   resources :videos
+
+  namespace :admin do 
+    root 'welcome#index'
+    resource :biography
+    resources :photos
+    resources :videos
+  end
 
 end

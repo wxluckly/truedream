e3a1c7091e5638d5700c1569eb64a2318d300d91
Joey wang <wxluckly@gmail.com>
2014-11-30 00:00:19 +0800

    coding

---
+++ app/controllers/admin/photos_controller.rb
@@ -0,0 +1,55 @@
+class Admin::PhotosController < Admin::BaseController
+
+  def index
+    @photos = Photo.paginate(page: params[:page]).order("id desc")
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to admin_photos_path }
+        format.json { render :json => @json }
+      else
+        format.html { render action: :edit }
+        format.json { render :json => @json }
+      end
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    flash[:notice] = 'success' if @photo.update_attributes(photo_params)
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to admin_photos_path }
+        format.js
+      else
+        format.html { render action: :edit }
+      end
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    flash[:notice] = "删除成功" if @photo.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_photos_path }
+      format.json { render json: {status: true}.to_json }
+    end
+  end 
+
+  private
+  def photo_params
+    params.require(:photo).permit(:sku, :price, :weight, :dimensions, :summary, :stock)
+  end
+
+end
---
+++ app/controllers/admin/videos_controller.rb
@@ -0,0 +1,55 @@
+class Admin::VideosController < Admin::BaseController
+
+  def index
+    @videos = Video.paginate(page: params[:page]).order("id desc")
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to admin_videos_path }
+        format.json { render :json => @json }
+      else
+        format.html { render action: :edit }
+        format.json { render :json => @json }
+      end
+    end
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    flash[:notice] = 'success' if @video.update_attributes(video_params)
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to admin_videos_path }
+        format.js
+      else
+        format.html { render action: :edit }
+      end
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    flash[:notice] = "删除成功" if @video.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_videos_path }
+      format.json { render json: {status: true}.to_json }
+    end
+  end 
+
+  private
+  def video_params
+    params.require(:video).permit(:sku, :price, :weight, :dimensions, :summary, :stock)
+  end
+
+end

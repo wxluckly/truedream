0e88a6de88d1f6beb01630249ecfc25c1c04eaa5
Joey wang <wxluckly@gmail.com>
2014-12-02 18:13:33 +0800

    coding

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -14,7 +14,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :on_use, :from_version => :big do
-    process resize_to_fill: [270, 389]
+    process resize_to_fill: [270, 390]
   end
 
   version :thumb, :from_version => :big do

169bc5496480ea3d435a39f827f231f75961aede
Joey wang <wxluckly@gmail.com>
2015-01-07 23:49:15 +0800

    coding

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,4 +2,8 @@ class NewsController < ApplicationController
   def index
     @news = News.sort_order.paginate(page: params[:page], per_page: 5)
   end
+
+  def show
+    @news = News.find(params[:id])
+  end
 end

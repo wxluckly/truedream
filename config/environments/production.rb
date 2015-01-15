240a2a92219479542c11af76a97f3a420e8238de
Joey wang <wxluckly@gmail.com>
2015-01-15 09:51:41 +0800

    coding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = Proc.new do |source|
-    "http://#{source =~ /(eot|otf|ttf|woff)$/ ? 'www' : 'cdn'}.uniwa.truedream.com.cn"
+    "http://#{source =~ /(eot|otf|ttf|woff)$/ ? 'www' : 'cdn'}.truedream.com.cn"
   end.tap {|p| def p.to_s; end}
 
   # Ignore bad email addresses and do not raise email delivery errors.
